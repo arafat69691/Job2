f6b647726c869b7bb148feba1cb4c40a785ad058
Yeasin Arafat <aparafat81@gmail.com>
2025-11-23 00:25:48 +0600

    initialize

---
+++ app/src/main/java/com/example/job2nsda/ProfileDetailsActivity.kt
@@ -0,0 +1,16 @@
+package com.example.job2nsda
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ProfileDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_profile_details)
+
+    }
+}
